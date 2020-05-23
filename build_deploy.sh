f588149f1b511741e869a95381b7eca8219b1eae
Kishore Devaraj <kishore.devaraj@reedexpo.co.uk>
2020-05-24 04:33:21 +0530

    Generate prod build

--- build_deploy.sh
+++ build_deploy.sh
@@ -5,5 +5,9 @@ test -d ~/.linuxbrew && eval $(~/.linuxbrew/bin/brew shellenv)
 test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
 
 echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.profile
+
 # Install hugo
 brew install hugo
+
+# Generate build
+hugo
