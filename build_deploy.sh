15a03265be50df1d8ef35de8e24882f5224c6f2c
Kishore Devaraj <kishore.devaraj@reedexpo.co.uk>
2020-05-24 04:27:54 +0530

    Install brew and add it to PATH

--- build_deploy.sh
+++ build_deploy.sh
@@ -1,5 +1,9 @@
-pwd
-cd curio
+# Install brew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
+test -d ~/.linuxbrew && eval $(~/.linuxbrew/bin/brew shellenv)
+test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+
+echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.profile
 # Install hugo
-brew install hugo
+#brew install hugo
